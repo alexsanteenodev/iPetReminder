23afcb7640b2d34838ddcacf5bbc757e33536158
Alex Santeeno <alexsanteeno@gmail.com>
2023-09-16 18:27:39 +0200

    Initial Commit

---
+++ iPetReminder/iPetReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPetReminderApp.swift
+//  iPetReminder
+//
+//  Created by Oleksandr Hanhaliuk on 16.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct iPetReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
