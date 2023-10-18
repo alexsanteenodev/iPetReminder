be2fe865be2b243ac0026bbfd138c6fd75a222e9
Alex Santeeno <alexsanteeno@gmail.com>
2023-10-18 23:34:24 +0200

    release 1.0

---
+++ iPetReminder/Common/GradientLayer+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  GradientLayer+Extension.swift
+//  iPetReminder
+//
+//  Created by Oleksandr Hanhaliuk on 30.09.2023.
+//
+
+import Foundation
---
+++ iPetReminder/Profile/Header.swift
@@ -0,0 +1,8 @@
+//
+//  Header.swift
+//  iPetReminder
+//
+//  Created by Oleksandr Hanhaliuk on 30.09.2023.
+//
+
+import Foundation
